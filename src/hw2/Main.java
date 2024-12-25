b6acfc24a940dbb722d1e40fd7f05bc0e2bac176
Ivan Hlukhenkyi <gluhenkiy.i@gmail.com>
2024-12-25 15:18:07 +0200

    code reformat

--- src/hw2/Main.java
+++ src/hw2/Main.java
@@ -4,30 +4,32 @@ import java.util.Scanner;
 
 public class Main {
     private static final double CONV_DIST = 1.609344;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter distance you want to convert(with miles or km): ");
         String input = scanner.nextLine();
+
         String dist = input.split(" ")[0];
         String unit = input.split(" ")[1].toLowerCase();
         double distance = Double.parseDouble(dist);
         double result = 0;
-        if (unit.equals("km")){
+
+        if (unit.equals("km")) {
             result = convertKmToMiles(distance);
             unit = "miles";
-        }
-        else if (unit.equals("miles")){
+        } else if (unit.equals("miles")) {
             result = convertMilesToKm(distance);
             unit = "km";
         }
-        System.out.println("Result: " + result+ " " + unit);
+        System.out.println("Result: " + result + " " + unit);
     }
 
-    private static double convertMilesToKm(double miles){
+    private static double convertMilesToKm(double miles) {
         return miles * CONV_DIST;
     }
 
-    private static double convertKmToMiles(double km){
+    private static double convertKmToMiles(double km) {
         return km / CONV_DIST;
     }
 }
