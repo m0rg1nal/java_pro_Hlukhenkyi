d9885a8242cc1f749427c9946a4f623ded89b0d0
Ivan Hlukhenkyi <gluhenkiy.i@gmail.com>
2024-12-28 14:28:18 +0200

    f to c conversion added

--- src/hw3/Main.java
+++ src/hw3/Main.java
@@ -1,7 +1,21 @@
 package hw3;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("TempConverter App");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter temperature you want to convert(with c or f): ");
+        String input = scanner.nextLine();
+
+        String temp = input.split(" ")[0];
+        String unit = input.split(" ")[1].toLowerCase();
+        double temperature = Double.parseDouble(temp);
+        double result = 0;
+        System.out.println(convertFtoC(temperature));
+    }
+    private static double convertFtoC(double temp){
+        return (temp - 32) / 1.8;
     }
 }
