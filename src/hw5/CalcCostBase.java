67c7067da85d3a5310ea135117eda243cac4c495
Ivan Hlukhenkyi <gluhenkiy.i@gmail.com>
2025-01-08 19:57:38 +0200

    code-to-fix added in hw5

--- src/hw5/CalcCostBase.java
+++ src/hw5/CalcCostBase.java
@@ -1,4 +1,11 @@
 package hw5;
 
+// Клас розрахунку базової вартості товару
+// ЗАВДАННЯ: Виправити код класу.
 public class CalcCostBase {
+
+    // Базовий розрахунок вартості товару
+    public double calcCost(Product product) {
+        return .getQuota() * product.;
+    }
 }
--- src/hw5/CalcCostDelivery.java
+++ src/hw5/CalcCostDelivery.java
@@ -1,4 +1,19 @@
 package hw5;
 
+// Клас розрахунку вартості товару,
+// враховуючи вартість доставки
+// ЗАВДАННЯ: Виправити код класу.
+// Клас успадковує клас CalcCostBase.
 public class CalcCostDelivery {
+
+    // Вартість доставки
+    private final static double deliveryPrice ;
+
+    // Розрахунок вартості товару,
+    // з урахуванням вартості доставки
+    @Override
+    public double calcCost(Product product) {
+        return product. product.getPrice()
+                + deliveryPrice;
+    }
 }
