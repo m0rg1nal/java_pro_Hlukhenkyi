8ac42a25626d4b6f415280dda31e6786e055f2d0
Ivan Hlukhenkyi <gluhenkiy.i@gmail.com>
2025-01-14 18:59:28 +0200

    hw7 code fixed

--- src/hw7/Corrector.java
+++ src/hw7/Corrector.java
@@ -1,4 +1,16 @@
 package hw7;
 
 public class Corrector {
+    public String handleData(String[] strs) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int count = 0;
+        for (String str : strs) {
+            if (str.contains("b")) {
+                str = str.replace("b", "o");
+            }
+            count++;
+            stringBuilder.append(count).append(") ").append(str).append("\n");
+        }
+        return stringBuilder.toString();
+    }
 }
--- src/hw7/Main.java
+++ src/hw7/Main.java
@@ -1,4 +1,12 @@
 package hw7;
 
 public class Main {
+    public static void main(String[] args) {
+        getOutput(new Corrector()
+                .handleData(new DataProvider().getData()));
+    }
+
+    private static void getOutput(String output) {
+        System.out.println(output);
+    }
 }
