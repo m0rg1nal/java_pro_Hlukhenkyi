9679a3bec35557aa29ed6cbedda519bcb78ec325
Ivan Hlukhenkyi <gluhenkiy.i@gmail.com>
2025-02-02 19:52:34 +0200

    hw11 fix

--- src/hw11/app/Main.java
+++ src/hw11/app/Main.java
@@ -1,4 +1,22 @@
 package hw11.app;
 
+import java.nio.file.Paths;
+
 public class Main {
+    private static final String BASE_PATH = "src/hw11/files/";
+
+    public static void main(String[] args) {
+        FileHandler handler = new FileHandler();
+        String newFileName = "myfile";
+        String content = "Super information.";
+        String path = BASE_PATH + newFileName + ".txt";
+        // Виклики методів маніпуляції з файлом
+        getOutput(handler.createFile(path));
+        getOutput(handler.writeToFile(Paths.get(path), content));
+        getOutput("CONTENT: " + handler.readFromFile(path));
+    }
+
+    private static void getOutput(String output) {
+        System.out.println(output);
+    }
 }
