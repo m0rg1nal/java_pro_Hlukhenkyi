a9ab0e5edbde0a512ff9f4d2e0a63b7f126b1315
Ivan Hlukhenkyi <gluhenkiy.i@gmail.com>
2025-02-05 20:59:53 +0200

    hw12 done

--- src/hw12/DataHandler.java
+++ src/hw12/DataHandler.java
@@ -1,4 +1,21 @@
 package hw12;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class DataHandler {
+    String[] fruits = new DataRepository().getData();
+
+    public void getOutput() {
+        // критичний блок коду
+        synchronized (this) {
+            StringBuilder sb = new StringBuilder();
+            AtomicInteger count = new AtomicInteger(1);
+            for (String fruit : fruits) {
+                sb.append(String.format("%d) %s ",
+                        count.getAndIncrement(), fruit));
+            }
+            System.out.println(MyThread.currentThread().getName() + ": " + sb);
+        }
+    }
 }
+
