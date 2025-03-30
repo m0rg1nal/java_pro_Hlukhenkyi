627dcc4f4f9a9c2950f16bfd14a103ceff6c4dc6
Ivan Hlukhenkyi <gluhenkiy.i@gmail.com>
2025-03-30 20:12:39 +0300

    hw24 done

--- src/hw24/Logger.java
+++ src/hw24/Logger.java
@@ -1,7 +1,11 @@
 package hw24;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Logger {
     private static Logger instance;
+    private List<String> logs = new ArrayList<>();
 
     private Logger() {}
 
@@ -12,6 +16,14 @@ public class Logger {
         return instance;
     }
     public void log(String msg){
+        logs.add("LOG: " + msg);
         System.out.println("LOG: " + msg);
+    }
+
+    public void getAllLogs() {
+        System.out.println("\nPrinting all logs");
+        for (String log : logs){
+            System.out.println(log);
+        }
     }
 }
